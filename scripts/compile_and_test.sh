aedb605ecbd91152f31eaca9c78d4dd24ea1c81e
Ferenc Béres <ferdzso05@gmail.com>
2015-07-14 11:58:43 +0200

    travis support added with compile script

---
+++ scripts/compile_and_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+thisDir="$(dirname $0)"
+thisDir="$(readlink -f "$thisDir")"
+
+pushd "$thisDir"
+
+pushd ../trunk
+# compile learning to rank source
+ant
+popd
+
+# run pytests
+../run_pytests.sh
+popd
