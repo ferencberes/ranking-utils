4baf7e3acc2bef38122140bc73edb38381049f0a
Beres Ferenc <fberes@ilab.sztaki.hu>
2015-07-15 10:32:06 +0200

    java_path_to_ranklib_runner

--- scripts/run_ranklib.sh
+++ scripts/run_ranklib.sh
@@ -8,6 +8,7 @@ from_interval=$5
 to_interval=$6
 metric="$7"
 output_folder="$8"
+java_home="$9"
 
 thisDir="$(dirname $0)"
 thisDir="$(readlink -f "$thisDir")"
@@ -21,7 +22,7 @@ for i in $(eval echo "{$from_interval..$to_interval}"); do
 	mkdir -p "$output_folder"/"$i"
 	output_file="$output_folder"/"$i"/"$file_prefix"_"$i"_"$feature_rank_type"_"$label_rank_type"_"$metric".out
 	# ranker 4 is Coordinate Ascent
-	java -jar trunk/bin/RankLib.jar -train "$train_file_name" -test "$test_file_name" -ranker 4 -metric2t "$metric" -metric2T "$metric" > "$output_file" &
+	$java_home/bin/java -jar trunk/bin/RankLib.jar -train "$train_file_name" -test "$test_file_name" -ranker 4 -metric2t "$metric" -metric2T "$metric" > "$output_file" &
 done
 wait
 echo "Running Experiments FINISHED."
